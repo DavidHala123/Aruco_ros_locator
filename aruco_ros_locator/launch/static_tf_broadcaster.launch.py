9c3d86f78738295e06278b896b137cd7606e1a53
DavidHala123 <78861269+DavidHala123@users.noreply.github.com>
2024-05-09 01:36:11 +0200

    Add files via upload

--- aruco_ros_locator/launch/static_tf_broadcaster.launch.py
+++ aruco_ros_locator/launch/static_tf_broadcaster.launch.py
@@ -6,10 +6,9 @@ from launch_ros.actions import Node
 def launch_setup(context, *args, **kwargs):
 
     aruco_locator_params = {
-        'file_has_child_frame': False,
+        'resend_when_subs_changed': True,
         'setup_file_path': LaunchConfiguration('setup_file_path'),
         'ref_frame_name': LaunchConfiguration('ref_frame_name'),
-        'cam_frame_name': LaunchConfiguration('cam_frame_name'),
     }
 
     aruco_locator = Node(
@@ -32,17 +31,12 @@ def generate_launch_description():
         description='Name of reference frame. '
     )
 
-    camera_frame_arg = DeclareLaunchArgument(
-        'cam_frame_name', default_value='camera',
-        description='Name of camera frame'
-    )
-    
+
     # Create the launch description and populate
     ld = LaunchDescription()
 
     ld.add_action(file_path_arg)
     ld.add_action(ref_frame_arg)
-    ld.add_action(camera_frame_arg) 
 
     ld.add_action(OpaqueFunction(function=launch_setup))
 
